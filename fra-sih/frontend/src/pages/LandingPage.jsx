a6b6f23a0d3140bbd81a36f747e163ad6f6b5aba
Vansh-Pandey <b24411@students.iitmandi.ac.in>
2025-09-17 16:10:48 +0530

    second commit

--- fra-sih/frontend/src/pages/LandingPage.jsx
+++ fra-sih/frontend/src/pages/LandingPage.jsx
@@ -9,44 +9,12 @@ import {
 } from "../components/ui/draggable-card";
 import AnimatedTooltip from "../components/ui/animated-tooltip"
 import people from "../data/team"
-export default function LandingPage() {
-    // FRA-related draggable cards data
-    const fraCards = [
-        {
-            title: "Forest Mapping",
-            image: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            className: "absolute top-10 left-[15%] rotate-[-8deg]",
-        },
-        {
-            title: "AI Analytics",
-            image: "https://images.unsplash.com/photo-1518173946687-a4c8892bbd9f?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            className: "absolute top-40 left-[25%] rotate-[-12deg]",
-        },
-        {
-            title: "GeoAI Detection",
-            image: "https://images.unsplash.com/photo-1501854140801-50d01698950b?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            className: "absolute top-5 left-[45%] rotate-[12deg]",
-        },
-        {
-            title: "WebGIS Platform",
-            image: "https://images.unsplash.com/photo-1508780709619-79562169bc64?auto=format&fit=crop&w=800&q=80",
-            className: "absolute top-32 left-[60%] rotate-[15deg]",
-        },
-        {
-            title: "Decision Support",
-            image: "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?auto=format&fit=crop&w=800&q=80",
-            className: "absolute top-20 right-[25%] rotate-[5deg]",
-        },
-        {
-            title: "Data Visualization",
-            image: "https://images.unsplash.com/photo-1526378722484-bd91ca387e72?auto=format&fit=crop&w=800&q=80",
-            className: "absolute top-24 left-[35%] rotate-[-10deg]",
-        },
-    ];
+import fraCards from "../data/fra_cards"
+export default function LandingPage() { 
 
     return (
         <div className="min-h-screen w-full bg-white dark:bg-black overflow-x-hidden">
-            <BackgroundLines className="flex flex-col items-center justify-start w-full px-4 py-20 relative space-y-32">
+            <BackgroundLines className="min-h-screen  flex flex-col items-center justify-start w-full px-4 py-12 md:py-20  relative space-y-12 md:space-y-32">
 
                 {/* Hero Section */}
                 <div className="flex flex-col items-center justify-center text-center w-full h-screen mx-auto -mt-20">
